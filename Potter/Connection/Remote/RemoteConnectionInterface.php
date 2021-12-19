bdc4ee5b1fa8586de787359778c2eafbb2af12bd
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-18 19:55:38 -0500

    user/pass getter/setter

--- Potter/Connection/Remote/RemoteConnectionInterface.php
+++ Potter/Connection/Remote/RemoteConnectionInterface.php
@@ -6,5 +6,11 @@ use Potter\Connection\ConnectionInterface;
 
 interface RemoteConnectionInterface extends ConnectionInterface
 {
-    
+    public function getPass(): string;
+
+    public function getUser(): string;
+
+    public function setPass(string $pass): void;
+
+    public function setUser(string $user): void;
 }
--- Potter/Connection/Remote/RemoteConnectionTrait.php
+++ Potter/Connection/Remote/RemoteConnectionTrait.php
@@ -6,13 +6,37 @@ trait RemoteConnectionTrait
 {
     private string $host;
 
+    private string $pass;
+    
+    private string $user;
+
     final public function getHost(): string
     {
         return $this->host;
     }
 
+    final public function getPass(): string
+    {
+        return $this->pass;
+    }
+
+    final public function getUser(): string
+    {
+        return $this->user;
+    }
+
     final public function setHost(string $host): void
     {
         $this->host = $host;
+    }
+
+    final public function setPass(string $pass): void
+    {
+        $this->pass = $pass;
+    }
+
+    final public function setUser(string $user): void
+    {
+        $this->user = $user;
     }
 }
