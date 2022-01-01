17f7cad6f57ed19e5b9029dae5ddfb95623744d4
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2022-01-01 17:37:10 -0500

    Update DatabaseConnectionInterface.php

--- Potter/Connection/Database/DatabaseConnectionInterface.php
+++ Potter/Connection/Database/DatabaseConnectionInterface.php
@@ -1,12 +1,23 @@
 <?php
 
-namespace Potter\Database\Connection;
+namespace Potter\Connection\Database;
 
 use Potter\{
-    Connection\ConnectionInterface
+    Connection\ConnectionInterface,
+    Database\DatabaseInterface
 };
 
 interface DatabaseConnectionInterface extends ConnectionInterface
 {
+    public function createDatabase(DatabaseInterface $database): void;
 
+    public function createDatabaseIfNotExists(DatabaseInterface $database): void;
+
+    public function databaseExists(string $database): bool;
+
+    public function getDatabase(string $database): DatabaseInterface;
+
+    public function getDatabases(bool $refresh = false): array;
+
+    public function getTables(string $database): array;
 }
